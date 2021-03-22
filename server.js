e81cbc13418c20c34e5fc64b3dc76c7e558119e3
ssearobber <ssearobber@naver.com>
2021-03-22 23:50:30 +0900

    update

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const { googleSheet } = require('./targetURLs/googleSheet');
 const schedule = require('node-schedule');
 require('dotenv').config(); 
 
-// 매일 아침 6시 갱신
-// schedule.scheduleJob('0 0 6 * * *', function () {
-//   googleSheet();
-// });
 googleSheet();
 // schedule.scheduleJob(process.env.SCHELDULE_TIME || schelduleTime, function () {
 //   googleSheet();
