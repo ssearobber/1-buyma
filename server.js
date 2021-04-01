c80a3fb8af2d2e1c6480a79e4981d328744e1ea6
ssearobber <ssearobber@naver.com>
2021-04-01 20:53:05 +0900

    update

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const schedule = require('node-schedule');
 require('dotenv').config(); 
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-//   googleSheet();
-// })
 
-// const port = process.env.PORT || 5050;
-// app.listen(port , () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+
+const port = process.env.PORT || 5050;
+app.listen(port , () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 
 schedule.scheduleJob(process.env.SCHELDULE_TIME || schelduleTime, function () {
-    console.log("매크로 실행");
-    googleSheet();
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+        console.log("매크로 실행");
+        googleSheet();
+    })
 });
