384202580163e6c4c9db3fc7a9161fecc71e339b
ssearobber <ssearobber@naver.com>
2021-04-04 16:40:10 +0900

    update

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const { googleSheet } = require('./targetURLs/googleSheet');
 require('dotenv').config(); 
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 // const schedule = require('node-schedule');
 
-googleSheet();
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-//   googleSheet();
-// })
+// googleSheet();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  googleSheet();
+})
 
-// const port = process.env.PORT || 5050;
-// app.listen(port , () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+const port = process.env.PORT || 5050;
+app.listen(port , () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 // schedule.scheduleJob(process.env.SCHELDULE_TIME || schelduleTime, async function () {
 //     console.log("매크로 실행");
