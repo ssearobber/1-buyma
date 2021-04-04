ed2fba38ef9aee13c0d9a9dea99b795b1b83dc36
ssearobber <ssearobber@naver.com>
2021-04-04 19:10:49 +0900

    update

--- targetURLs/buyma.js
+++ targetURLs/buyma.js
@@ -140,7 +140,7 @@ async function buyma(row) {
     });
     const[fileChooser] = await Promise.all([
         page.waitForFileChooser(),
-        page.click('.bmm-c-img-upload__dropzone'),
+        page.click('.bmm-c-img-upload__dropzone.bmm-c-img-upload__dropzone--cover'),
     ])
     console.log("imagePathArray 확인", imagePathArray);
     await fileChooser.accept(imagePathArray);
